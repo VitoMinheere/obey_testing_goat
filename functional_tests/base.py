6cb7923610c1e620cddb4b39152beb9e2882bd16
Vito <you@example.com>
2021-01-09 16:51:15 +0100

    Adjust new list view to do model validation

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -2,8 +2,6 @@ import os
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
-from selenium.webdriver.common.keys import Keys
-from unittest import skip
 import time
 
 MAX_WAIT = 10
@@ -28,6 +26,16 @@ class FunctionalTest(StaticLiveServerTestCase):
                 rows = table.find_elements_by_tag_name('tr')
                 self.assertIn(row_text, [row.text for row in rows])
                 return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+
+    def wait_for(self, fn):
+        start_time = time.time()
+        while True:
+            try:
+                return fn()
             except (AssertionError, WebDriverException) as e:
                 if time.time() - start_time > MAX_WAIT:
                     raise e
