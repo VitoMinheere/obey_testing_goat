acf475b3ba35d475f538cf51b1aa59020eb47cff
Vito <you@example.com>
2021-01-09 17:51:37 +0100

    Use get_absolute_url on List model

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -12,10 +12,10 @@ class ItemValidationTest(FunctionalTest):
 
         # The home page refreshes, and there is an error message saying
         # that list items cannot be blank
-        self.assertEqual(
+        self.wait_for(lambda: self.assertEqual(
                 self.browser.find_element_by_css_selector('.has-error').text,
                 "You can't have an empty list item"
-        )
+        ))
 
         # She tries again with some text for the item, which now works
         self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
