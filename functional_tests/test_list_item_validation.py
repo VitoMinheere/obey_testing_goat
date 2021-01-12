60ade3b45fc2a502dda29d54653945ab31085bea
Vito <you@example.com>
2021-01-12 08:47:39 +0100

    Implement duplicate item validation at model layer

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -14,13 +14,13 @@ class ItemValidationTest(FunctionalTest):
         # list page
         self.wait_for(lambda: self.browser.find_elements_by_css_selector(
             '#id_text:invalid'
-        ))
+            ))
 
         # She starts typing some text for the new item and the error disappears
         self.get_item_input_box().send_keys('Buy milk')
         self.wait_for(lambda: self.browser.find_element_by_css_selector(
             '#id_text:valid')
-                )
+            )
 
         # She tries again with some text for the item, which now works
         self.get_item_input_box().send_keys(Keys.ENTER)
@@ -31,18 +31,33 @@ class ItemValidationTest(FunctionalTest):
 
         # Again, the browser will not comply
         self.wait_for_row_in_list_table('1: Buy milk')
-        self.wait_for(lambda:
-            self.browser.find_elements_by_css_selector(
-            '#id_text:invalid'
-
-        ))
-
+        self.wait_for(lambda: self.browser.find_elements_by_css_selector(
+                    '#id_text:invalid'
+                    )
+                )
 
         # And she can correct it by filling some text in
         self.get_item_input_box().send_keys('Make tea')
         self.wait_for(lambda: self.browser.find_elements_by_css_selector(
             '#id_text:valid'
-        ))
+            ))
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
         self.wait_for_row_in_list_table('2: Make tea')
+
+    def test_cannot_add_duplicate_item(self):
+        # Edith goes to the home page and starts a new list
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys('Buy wellies')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy wellies')
+
+        # She accidentally tries to enter a duplicate item
+        self.get_item_input_box().send_keys('Buy wellies')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        # She sees a helpful error message
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "You've already got this in your list"
+        ))
