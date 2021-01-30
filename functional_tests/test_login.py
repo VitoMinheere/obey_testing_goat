2531eb66c78fdb7fe816332cb2a74cfb85ba6110
Vito <you@example.com>
2021-01-30 12:50:14 +0100

    Add command for FT

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -1,16 +1,22 @@
 from django.core import mail
 from selenium.webdriver.common.keys import Keys
+import os
+import poplib
 import re
+import time
 
 from .base import FunctionalTest
 
-TEST_EMAIL = 'edith@example.com'
 SUBJECT = 'Your login link for Superlists'
 
 
 class LoginTest(FunctionalTest):
 
     def test_can_get_email_link_to_log_in(self):
+        if self.staging_server:
+            TEST_EMAIL = "jayjanssens896@gmail.com"
+        else:
+            TEST_EMAIL = 'edith@example.com'
         # Edith goes to the awesome superlists site
         # and notices a "Log in" section in the navbar for the first time
         # It's telling her to enter her email address, so she does
@@ -25,15 +31,13 @@ class LoginTest(FunctionalTest):
         ))
 
         # She checks her email and finds a message
-        email = mail.outbox[0]
-        self.assertIn(TEST_EMAIL, email.to)
-        self.assertEqual(email.subject, SUBJECT)
+        body = self.wait_for_email(TEST_EMAIL, SUBJECT)
 
         # It has a url link in it
-        self.assertIn('Use this link to log in', email.body)
-        url_search = re.search(r'http://.+/.+$', email.body)
+        self.assertIn('Use this link to log in', body)
+        url_search = re.search(r'http://.+/.+$', body)
         if not url_search:
-            self.fail(f'Could not find url in email body:\n{email.body}')
+            self.fail(f'Could not find url in email body:\n{body}')
         url = url_search.group(0)
         self.assertIn(self.live_server_url, url)
 
@@ -48,3 +52,34 @@ class LoginTest(FunctionalTest):
 
         # She is logged out
         self.wait_to_be_logged_out(email=TEST_EMAIL)
+
+    def wait_for_email(self, test_email, subject):
+        if not self.staging_server:
+            email = mail.outbox[0]
+            self.assertIn(test_email, email.to)
+            self.assertEqual(email.subject, subject)
+            return email.body
+
+        email_id = None
+        start = time.time()
+        inbox = poplib.POP3_SSL('pop.gmail.com')
+        try:
+            inbox.user(test_email)
+            inbox.pass_(os.environ['EMAIL_PASSWORD'])
+            while time.time() - start < 60:
+                # get 10 newest messages
+                count, _ = inbox.stat()
+                for i in reversed(range(max(1, count - 10), count + 1)):
+                    print('getting msg', i)
+                    _, lines, __ = inbox.retr(i)
+                    lines = [line.decode('utf8') for line in lines]
+                    print(lines)
+                    if f'Subject: {subject}' in lines:
+                        email_id = i
+                        body = '\n'.join(lines)
+                        return body
+                time.sleep(5)
+        finally:
+            if email_id:
+                inbox.dele(email_id)
+            inbox.quit()
--- superlists/settings.py
+++ superlists/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'lists',
     'accounts',
+    'functional_tests',
 ]
 
 AUTH_USER_MODEL = 'accounts.User'
@@ -134,3 +135,21 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
+# Logging to console
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
+    },
+    'root': {'level': 'INFO'},
+}
