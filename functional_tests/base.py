cb8744e034fedbfbaa5749ade1a5ec2f02bc779b
Vito <you@example.com>
2021-02-07 20:28:57 +0100

    New FT for sharing, move session creation stuff to base

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -116,8 +116,8 @@ class FunctionalTest(StaticLiveServerTestCase):
         else:
             session_key = create_pre_authenticated_session(email)
 
-        ## to set a cookie we need to first visit the domain.
-        ## 404 pages load the quickest!
+        # to set a cookie we need to first visit the domain.
+        # 404 pages load the quickest!
         self.browser.get(self.live_server_url + "/404_no_such_url/")
         self.browser.add_cookie(dict(
             name=settings.SESSION_COOKIE_NAME,
